0a1023395f8136307db13e4b7e029cf30af39dc5
kirtimish <kirtimishra0708@gmail.com>
2022-12-12 04:37:21 +0530

    integreating TS with node and express

---
+++ node-typescript/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import todosRoutes from './routes/todos'
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(todosRoutes);
+app.use(bodyParser.json());
+app.listen(3000)
